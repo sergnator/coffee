abf6e103197fe60fef4755599f3037dfbcffb79b
sergnator <86975413+sergnator@users.noreply.github.com>
2023-11-17 21:55:06 +0300

    add table

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import sqlite3
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5 import uic
 
 
 class Coffee(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.view = QTableWidget()
         uic.loadUi('main.ui', self)
+        self.fill_table()
+
+    def fill_table(self):
+        db = QSqlDatabase.addDatabase('SQLITE')
+        db.setDatabaseName('coffe.sqlite')
+        db.open()
+        model = QSqlTableModel(self, db)
+        model.setTable('coffe')
+        model.select()
+
+        self.view.setModel(model)  # todo исправить таблицу
 
 
 if __name__ == "__main__":
